1590c7b07bad8672af6bab7a39dbd50a71c768ae
Andrea <andrea.axelsson94@hotmail.com>
2023-05-31 15:02:21 +0200

    Commit initial files

---
+++ index.js
@@ -0,0 +1,14 @@
+function aboutMeFunction()
+{
+    window.location="about-me.html"
+}
+
+function homeFunction()
+{
+    window.location="index.html"
+}
+
+function blogPostFunction()
+{
+    window.location="blog-post.html"
+}
